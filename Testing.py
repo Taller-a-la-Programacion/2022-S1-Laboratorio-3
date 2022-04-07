ae035345a575126753e6797dd54f8690014cc9f6
Cristian Campos Agüero <ccampos04@gmail.com>
2022-04-07 13:10:44 -0600

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -2,10 +2,10 @@ import Laboratorio3;
 import pytest;
     
 def test_eliminarRepetidos_1():
-    assert Laboratorio5.eliminarSegundaRepeticion( [12, 78, 12, 0, 5.2, "abc", 0, 12, 5.2, 12] ) == [12, 78, 12, 0, 5.2, "abc", 0, 5.2]
+    assert Laboratorio5.eliminarRepetidos( [12, 78, 12, 0, 5.2, "abc", 0, 12, 5.2, 12] ) == [78, "abc"]
     
 def test_eliminarRepetidos_2():
-    assert Laboratorio5.eliminarSegundaRepeticion( [12, 5.2, 12] ) == [12, 5.2, 12]
+    assert Laboratorio5.eliminarRepetidos( [12, 5.2, 12] ) == [5.2]
     
 def test_sumaImparesPares_1():
     assert Laboratorio5.sumaImparesPares([0,2,3,4], [4, 8, 6, 0]) == [13, 14]
